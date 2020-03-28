b64d37e6cb61c56abc0e9ec2928d5a3f5faa51b5
bdandurand <bdandurand@anl.gov>
2020-03-28 12:19:44 -0500

    Now the example allows to refer to lower-level C interface of CPLEX, all output suppressed, other parameter setting examples coded out.

--- Smi/examples/siplib.cpp
+++ Smi/examples/siplib.cpp
@@ -41,92 +41,36 @@ void SmpsIO(const char * const name )
 		//	<name>.core, <name>.time, and <name>.stoch
 		smi.readSmps(name);
 
-		// generate OSI solver object
-		// 	here we use OsiClp
-#if 0
-		OsiCpxSolverInterface *clp = new OsiCpxSolverInterface();
-        smi.solveWS(clp, 1.0);
-
-
-		// set solver object for SmiScnModel
-		smi.setOsiSolverHandle(*clp);
-
-		// load solver data
-		// 	this step generates the deterministic equivalent
-		//	and returns an OsiSolver object
-		OsiSolverInterface *osiStoch = smi.loadOsiSolverData();
-
-		// set some nice Hints to the OSI solver
-		osiStoch->setHintParam(OsiDoPresolveInInitial,true);
-		osiStoch->setHintParam(OsiDoScale,true);
-		osiStoch->setHintParam(OsiDoCrash,true);
-        OsiCpxSolverInterface *osi_cpx = reinterpret_cast<OsiCpxSolverInterface*>(osiStoch);
-        CPXsetintparam( osi_cpx->getEnvironmentPtr(), CPXPARAM_Threads, 0);
-
-		// solve
-		osiStoch->initialSolve();
-        osiStoch->branchAndBound();
-
-		// print results
-		printf("Solved stochastic program %s\n", name);
-		printf("Number of rows: %d\n",osiStoch->getNumRows());
-		printf("Number of cols: %d\n",osiStoch->getNumCols());
-		printf("Optimal value: %g\n",osiStoch->getObjValue());
-
-		// print solution to file
-		string outfilename(name);
-		const string suffix(".out");
-		outfilename = outfilename + suffix;
-		FILE *fp = fopen(outfilename.c_str(),"w");
-#endif
-        //OsiCpxSolverInterface *osi_cpx = new OsiCpxSolverInterface();
-        //double result = smi.getWSValue(osi_cpx, 1.0 );
-        //cout << "Result: " << result << endl;
 		int numScenarios=smi.getNumScenarios();
 
-        OsiSolverInterface **osi_cpx_scns = new OsiSolverInterface*[numScenarios];
-		for (int ii=0 ; ii<numScenarios; ++ii) {
-#if 0
-			double *dsoln=NULL;
-			int numCols=0;
-			fprintf(fp,"Scenario %d \n",ii);
-			dsoln = smi.getColSolution(ii,&numCols);
-			for (int jj=0; jj<numCols; jj++)
-				fprintf(fp,"%g \n",dsoln[jj]);
-			free(dsoln);
-#endif
+        OsiCpxSolverInterface **osi_cpx_scns = new OsiCpxSolverInterface*[numScenarios];
+		for (int ii=0 ; ii<numScenarios; ++ii){
             cout << "Subproblem: " << ii << endl;
             osi_cpx_scns[ii] = new OsiCpxSolverInterface();
-            osi_cpx_scns[ii] = smi.loadOsiSolverDataForScenarioSP(osi_cpx_scns[ii], ii);
-            //osi_cpx_scns[ii] = dynamic_cast<OsiCpxSolverInterface*>(osi_cpx_scns[ii]);
-#if 0
-        for (int mi = 0; mi < osi_cpx_scns[ii]->getMatrixByRow()->getMinorDim();mi++){
-            std::cout << osi_cpx_scns[ii]->getObjCoefficients()[mi] << "*x" << mi << " + ";
-        }
-        std::cout << std::endl;
-        printf("\n Objective printed \n");
-        for (int mi = 0; mi < osi_cpx_scns[ii]->getMatrixByRow()->getMajorDim(); mi++){
-            printf("\n%g <= ", osi_cpx_scns[ii]->getRowLower()[mi]);
-            for (int mj = 0; mj < osi_cpx_scns[ii]->getMatrixByRow()->getMinorDim(); mj++){
-                printf("%g ",osi_cpx_scns[ii]->getMatrixByRow()->getCoefficient(mi,mj));
-            }
-            printf("<= %g",osi_cpx_scns[ii]->getRowUpper()[mi]);
-        }
-        printf("\n Matrix printed \n");
-        for (int mi = 0; mi < osi_cpx_scns[ii]->getNumCols(); mi++) {
-            printf("\n%g <= x%d <= %g",osi_cpx_scns[ii]->getColLower()[mi],mi,osi_cpx_scns[ii]->getColUpper()[mi]);
-        }
-        printf("\n");
-        printf("\n Var bounds printed \n");
-#endif
-            // Set objSense
-            osi_cpx_scns[ii]->setObjSense(1.0);
-            //char fname[128];
-            //sprintf(fname,"LpFiles/lp%d",ii);
-            //cout << fname << endl;
-            //osi_cpx_scns[ii]->writeLp(fname);
-            //CPXsetintparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_Threads, 0);
-            //CPXsetintparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_ScreenOutput, 0);
+            smi.loadOsiSolverDataForScenarioSP(osi_cpx_scns[ii], ii);
+            osi_cpx_scns[ii]->setObjSense(1.0);  // Set objective sense, MIN=1.0
+	        osi_cpx_scns[ii]->messageHandler()->setLogLevel(0); //Nice hack to suppress all output
+
+            int status=0;
+            int nThreads=1;  //Might want to experiment with this if you're using the more recent versions of CPLEX
+            int MIP_TOL=1e-6;
+            status=CPXsetintparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_Threads, 1);
+	        status=CPXsetintparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_Parallel, CPX_PARALLEL_DETERMINISTIC); //no iteration message until solution
+	        status=CPXsetintparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_Threads, nThreads); //no iteration message until solution
+            // Tolerance relevant parameters
+            #if 0
+	        status=CPXsetdblparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_MIP_Tolerances_AbsMIPGap, MIP_TOL);
+	        status=CPXsetdblparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_MIP_Tolerances_MIPGap, MIP_TOL*1e-3);
+	        status=CPXsetdblparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_MIP_Tolerances_Integrality, MIP_TOL);
+	        status=CPXsetdblparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_Simplex_Tolerances_Optimality, MIP_TOL);
+	        status=CPXsetdblparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_Simplex_Tolerances_Feasibility, MIP_TOL);
+            #endif
+            #if 0
+            status=CPXsetintparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_Simplex_Display, 0); //no iteration message until solution
+            status=CPXsetintparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_Tune_Display, 0); //turn off display
+            status=CPXsetintparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPX_PARAM_SCRIND, CPX_OFF); //turn off display
+            status=CPXsetintparam( osi_cpx_scns[ii]->getEnvironmentPtr(), CPXPARAM_Barrier_Display, 0); //turn off display
+            #endif
 
 		    osi_cpx_scns[ii]->initialSolve();
             osi_cpx_scns[ii]->branchAndBound();
@@ -135,7 +79,6 @@ void SmpsIO(const char * const name )
             //delete osi_cpx_scns[ii];
             cout << "End of subproblem: " << ii << endl;
 		}
-		//fclose(fp);
         //delete [] osi_cpx_scns;
 
 }
